0095764eb3e88f1e12f4095e16c736077b6ec3a8
ivanjosueariasastua <53876880+ivanjosueariasastua@users.noreply.github.com>
2023-05-16 10:16:13 -0600

    Login creado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '',
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
